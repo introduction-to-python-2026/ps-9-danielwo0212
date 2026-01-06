9508bf657ee09d61d0aad47682da28cb3693c4f1
danielwo0212 <danielwo0212@gmail.com>
2026-01-07 00:01:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import pandas as pd
 
+df = pd.read_csv("parkinsons.csv")
+df = df.dropna()
+
+print(df.columns.to_list())
+
+features = ['spread1', 'spread2']
+x = df[features]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=14)
+model.fit(x_train,y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+accuracy
+
+import joblib
+
+joblib.dump(model, 'my_model.joblib')
